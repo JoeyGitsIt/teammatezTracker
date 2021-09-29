cff31bf6d9966278c9425a707d252bdb7c87c3cf
JoeyGitsIt <josephperri3@gmail.com>
2021-09-29 13:32:49 -0400

    added variable

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-// This is a test im not really sure what will happen
+// setting up localStorage
+// arguements that the api can take: name, accountType(epic/psn/xbl), timeWindow(season/lifetime), image (displays type of controller being used)
+
+var apiLink = "https://fortnite-api.com/v2/stats/br/v2"
+
+
+function getInformation() {
+  // get value of input box
+  var initials = initialsEl.value.trim();
+
+  // make sure value wasn't empty
+  if (initials !== "") {
+    // get saved scores from localstorage, or if not any, set to empty array
+    var highscores =
+      JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    // format new score object for current user
+    var newScore = {
+      score: time,
+      initials: initials
+    };
+
+    // save to localstorage
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+    // redirect to next page
+    window.location.href = "highscores.html";
+  }
+}
