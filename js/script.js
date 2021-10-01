05d18675dbc8183437d75f7fae7cb16e04b44e38
Will Gresham <willgresham34@gmail.com>
2021-10-01 11:13:27 -0400

    Charts started

--- js/script.js
+++ js/script.js
@@ -137,3 +137,80 @@ function setHistory(apiResponse) {
 getHistory();
 
 //   name: "skxawng6",
+
+
+
+
+
+
+var kdctx = document.getElementById('kdChart').getContext('2d');
+var kdChart = new Chart(kdctx, {
+    type: 'bar',
+    data: {
+        labels: ['Current Player', 'Rp 1', 'Rp 2', 'Rp 3', 'Rp 4', 'Rp 6', ' rp 7', 'rp8', 'rp 9', 'rp10'],
+        datasets: [{
+
+            data: [12, 19, 3, 5, 2, 15, 3, 3, 3, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+var winctx = document.getElementById('winpChart').getContext('2d');
+var winpChart = new Chart(winctx, {
+    type: 'bar',
+    data: {
+        labels: ['Current Player', 'Rp 1', 'Rp 2', 'Rp 3', 'Rp 4', 'Rp 6', ' rp 7', 'rp8', 'rp 9', 'rp10'],
+        datasets: [{
+
+            data: [12, 10, 3, 5, 2, 10, 3, 3, 3, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
