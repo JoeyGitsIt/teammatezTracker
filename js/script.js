05635ef941ef8d004555d12f5392de8c02a84e82
JoeyGitsIt <josephperri3@gmail.com>
2021-10-01 10:34:10 -0400

    wip Local Storage

--- js/script.js
+++ js/script.js
@@ -108,7 +108,6 @@ $(function () {
 var historyParsed = "";
 
 
-<<<<<<< HEAD
 // function getHistory() {
 //   get value of input box
 //   $.ajax({
@@ -146,33 +145,6 @@ function setHistory(apiResponse) {
     // historyParsed = JSON.parse(localStorage.getItem("history"));
     // console.log(historyParsed[0].data.account.name);
   }
-=======
-function getHistory() {
-    // get value of input box
-    $.ajax({
-        url: apiLink,
-        method: 'GET',
-    }).then(function (response) {
-        console.log('Ajax Reponse \n-------------');
-        setHistory(response);
-        console.log(response);
-    })
-}
-
-
-function setHistory(apiResponse) {
-    if (apiResponse.status == 200) {
-        var history = JSON.parse(window.localStorage.getItem("history")) || [];
-        console.log(history);
-
-        // if input is equal to a name already in the search history, do not push that names apiCall onto the JSON localStorage object thing
-        history.push(apiResponse);
-        window.localStorage.setItem("history", JSON.stringify(history));
-        // historyParsed stores the array of JSON objects Pog
-        // historyParsed = JSON.parse(localStorage.getItem("history"));
-        // console.log(historyParsed);
-    }
->>>>>>> 37b87b63b4264ef93227bb61351549aa28592896
 }
 
 
